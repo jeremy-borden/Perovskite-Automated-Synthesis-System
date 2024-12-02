045313c7bd4bc4ece437d2c664dcceb6a8231b50
Jem <46539847+jeremy-borden@users.noreply.github.com>
2024-12-02 14:27:04 -0500

    Update whatever.py

--- src/whatever.py
+++ src/whatever.py
@@ -1,55 +1,117 @@
+from cProfile import label
 from calendar import c
 from re import S
+from tkinter import E
 from turtle import st
 import customtkinter as ctk
 from numpy import pad
 
+LABEL_WIDTH = 100
+
 class ProcedureFrame(ctk.CTkScrollableFrame):
     def __init__(self, master):
-        super().__init__(master, width=400, height=400)
+        super().__init__(master, width=450, height=800)
         
         self.titleLabel = ctk.CTkLabel(self, text="Procedure", justify="left", font=("Arial", 14, "bold")).grid(row=0, column=0)
         
-        hf = HeatStep(self).grid(row=1, column=0, sticky = "nw", padx=10, pady=10)
-        sf = SpinStep(self).grid(row=2, column=0, sticky = "nw", padx=10, pady=10)
-        af = ApplyStep(self).grid(row=3, column=0, sticky = "nw", padx=10, pady=10)
-        mf = MixStep(self).grid(row=4, column=0, sticky = "nw", padx=10, pady=10)
+        self.btn_frame = ctk.CTkFrame(self, height=100, border_color="#1f6aa5", border_width=2)
+        self.btn_frame.grid(row=1, column=0, sticky = "nwe", padx=10, pady=10)
+        self.add_step_button = ctk.CTkButton(self.btn_frame, text="Add Step", width = 20, height = 20)
+        self.add_step_button.grid(row=1, column=0, sticky = "nw", padx=10, pady=10)
+
+        
+        hf = HeatStep(self).grid(row=2, column=0, sticky = "nw", padx=10, pady=10)
+        sf = SpinStep(self).grid(row=3, column=0, sticky = "nw", padx=10, pady=10)
+        af = ApplyStep(self).grid(row=4, column=0, sticky = "nw", padx=10, pady=10)
+        mf = MixStep(self).grid(row=5, column=0, sticky = "nw", padx=10, pady=10)
+        
+    
         
 class StepFrame(ctk.CTkFrame):
     def __init__(self, master, title):
         super().__init__(master, border_color="#1f6aa5", border_width=2, width=400, height=100)
-        
-        self.label_width = 200
-        self.titleLabel = ctk.CTkLabel(self, text=title, justify="left", font=("Arial", 14, "bold")).grid(row=0, column=0, sticky = "nw")
+        self.bind("<Button-1>", command = self.onFocusIn)
+  
+        pad = 5
+        self.titleLabel = ctk.CTkLabel(self, text=title, justify="left", font=("Arial", 14, "bold")).grid(row=0, column=0, sticky = "nw", padx=pad, pady=pad)
         
     def createValueEntryLabel(self, label_text):
         frame = ctk.CTkFrame(self)
-        self.value_entry_label = ctk.CTkLabel(frame, text=label_text, justify="left", width=self.label_width, anchor="w").grid(row=0, column=0, sticky="nw")
+        self.value_entry_label = ctk.CTkLabel(frame, text=label_text, justify="left", width=LABEL_WIDTH, anchor="w").grid(row=0, column=0, sticky="nw")
         self.value_entry = ctk.CTkEntry(frame, width=50, placeholder_text="...").grid(row=0, column=1, sticky="nw")
         return frame
     
     def createDropdownLabel(self, label_text, options):
         frame = ctk.CTkFrame(self)
-        self.dropdown_label = ctk.CTkLabel(frame, text=label_text, justify="left", width=self.label_width, anchor="w").grid(row=0, column=0, sticky="nw")
+        self.dropdown_label = ctk.CTkLabel(frame, text=label_text, justify="left", width=LABEL_WIDTH, anchor="w").grid(row=0, column=0, sticky="nw")
         self.dropdown = ctk.CTkOptionMenu(frame, values=options, width = 50, height = 20).grid(row=0, column=1, sticky="nw")
         return frame
     
+    def onFocusIn(self, event):
+        print(f"Focus")
+        
+
+class EntryLabel(ctk.CTkFrame):
+    def __init__(self, master, label_text):
+        super().__init__(master)
+        pad = 5
+
+        self.entry_label = ctk.CTkLabel(self, text=label_text, justify="left", width=LABEL_WIDTH, anchor="w")
+        self.entry_label.grid(row=0, column=0, sticky="nw", padx=pad, pady=pad)
+        self.entry = ctk.CTkEntry(self, width=50, placeholder_text="...")
+        self.entry.grid(row=0, column=1, sticky="nw", padx=pad, pady=pad)
+        
+    def get_value(self):
+        return self.entry.get()
     
+    def set_value(self, value): 
+        self.entry.delete(0, ctk.END)
+        self.entry.insert(0, value)
+
+class DropdownLabel(ctk.CTkFrame):
+    def __init__(self, master, label_text, options):
+        super().__init__(master, border_color="#1f6aa5", border_width=2, width=400, height=100)
+        self.dropdown_label = ctk.CTkLabel(self, text=label_text, justify="left", width=LABEL_WIDTH, anchor="w")
+        self.dropdown_label.grid(row=0, column=0, sticky="nw")
+        self.dropdown = ctk.CTkOptionMenu(self,values=options, width = 50, height = 20)
+        self.dropdown.grid(row=0, column=1, sticky="nw")
+    
+    def get_value(self):
+        return self.dropdown.get()
+    
+    def set_value(self, value):
+        self.dropdown.set(value)
+    
+
     
     
 class HeatStep(StepFrame):
     def __init__(self, master):
         super().__init__(master, "HEAT STEP")
+        self.value_dict = {"bake_temperature": None, "bake_time": None}
         
-        self.createValueEntryLabel("Bake Temperature: ").grid(row=1, column=0, sticky = "nw")
-        self.createValueEntryLabel("Bake Time: ").grid(row=2, column=0, sticky = "nw")
+        self.bake_temperature = EntryLabel(self, "Bake Temperature: ")
+        self.bake_temperature.grid(row=1, column=0, sticky = "nw", padx=10, pady=10)
+        
+        self.bake_time = EntryLabel(self, "Bake Time: ")
+        self.bake_time.grid(row=2, column=0, sticky = "nw", padx=10, pady=10)
+    
+    def get_values(self):
+        self.value_dict["bake_temperature"] = self.bake_temperature.entry.get()
+        self.value_dict["bake_time"] = self.bake_time.entry.get()
+        return self.value_dict
+        
+   
 
 class SpinStep(StepFrame):
     def __init__(self, master):
         super().__init__(master, "SPIN STEP")
+
         
         self.createValueEntryLabel("Spin Speed: ").grid(row=1, column=0, sticky = "nw")
         self.createValueEntryLabel("Spin Time: ").grid(row=2, column=0, sticky = "nw")
+        
+
 
 class ApplyStep(StepFrame):
     def __init__(self, master):
@@ -93,7 +155,7 @@ class MixStep(StepFrame):
     
         
         
-class ProcedureHandeler():
+class StepManager():
     def __init__(self):
         self.steps = {}
         
@@ -106,6 +168,10 @@ class ProcedureHandeler():
         
     def insertStep(self, step, index):
         self.steps.insert(index, step)
+
+
+
+
 
 if __name__ == "__main__":
     app = ctk.CTk()
