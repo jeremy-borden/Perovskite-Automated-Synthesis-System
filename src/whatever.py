c7cfe923175b347364278d70449979de4f86b359
Jem <46539847+jeremy-borden@users.noreply.github.com>
2024-12-02 18:51:58 -0500

    Update whatever.py

--- src/whatever.py
+++ src/whatever.py
@@ -1,34 +1,40 @@
-from cProfile import label
-from calendar import c
-from re import S
-from tkinter import E
-from turtle import st, up
 import customtkinter as ctk
-from numpy import pad
+import abc
 
 LABEL_WIDTH = 150
 
 class ProcedureFrame(ctk.CTkScrollableFrame):
     def __init__(self, master):
-        super().__init__(master, width=450, height=800)
+        super().__init__(master, width=450, height=500)
         self.step_frames = []
         
         self.titleLabel = ctk.CTkLabel(self, text="Procedure", justify="left", font=("Arial", 14, "bold")).grid(row=0, column=0)
         
         self.btn_frame = ctk.CTkFrame(self, height=100, border_color="#1f6aa5", border_width=2)
         self.btn_frame.grid(row=1, column=0, sticky = "nwe", padx=10, pady=10)
+        
         self.add_step_button = ctk.CTkButton(self.btn_frame, text="Add Step", width = 20, height = 20, command=self.onAddStep)
         self.add_step_button.grid(row=1, column=0, sticky = "nw", padx=10, pady=10)
+        
         self.step_dropdown = ctk.CTkOptionMenu(self.btn_frame, width = 50, height = 20, values=["Heat", "Spin", "Apply", "Mix"])
         self.step_dropdown.grid(row=1, column=1, sticky = "nw", padx=10, pady=10)
+        
         self.delete_step_button = ctk.CTkButton(self.btn_frame, text="Delete Step", width = 20, height = 20, command=self.onRemoveStep)
         self.delete_step_button.grid(row=1, column=2, sticky = "nw", padx=10, pady=10)
-    
-        def get_steps(self):
-            steps = []
-            for step in self.step_frames:
-                steps.append(step)
-            return steps
+        
+        self.export_button = ctk.CTkButton(self.btn_frame, text="Export", width = 20, height = 20, command=self.exportProcedure)
+        self.export_button.grid(row=1, column=3, sticky = "nw", padx=10, pady=10)
+        
+    def get_steps(self):
+        steps = []
+        for step in self.step_frames:
+            steps.append(step.get_values())
+        return steps
+        
+    def exportProcedure(self):
+        steps = self.get_steps()
+        print(steps)
+        
             
             
     def onAddStep(self):
@@ -56,19 +62,26 @@ class ProcedureFrame(ctk.CTkScrollableFrame):
         self.step_frames.pop().destroy()
         
 class StepManager():
-    def __init__(self, procedure_frame: ProcedureFrame):
+    def __init__(self):
         self.steps = {}
-        self.frame = procedure_frame
+      
     
-    def addStep(self, step):
-        self.steps.append(step)
+    def decodeStep(self, step_dict):
+        step_type = step_dict["step_type"]
         
-    def removeStep(self, step, index):
-        self.steps.pop(index)
-        
-    def insertStep(self, step, index):
-        self.steps.insert(index, step)
-
+        match step_type:
+            case "heat":
+                temperature = step_dict["temperature"]
+                duration = step_dict["duration"]
+                
+            case "spin":
+                step = SpinStep(self.frame)
+            case "apply":
+                step = ApplyStep(self.frame)
+            case "mix":
+                step = MixStep(self.frame)
+            case _:
+                step = None
 
         
 class StepFrame(ctk.CTkFrame):
@@ -78,22 +91,13 @@ class StepFrame(ctk.CTkFrame):
   
         pad = 5
         self.titleLabel = ctk.CTkLabel(self, text=title, justify="left", font=("Arial", 14, "bold")).grid(row=0, column=0, sticky = "nw", padx=pad, pady=pad)
-        
-    def createValueEntryLabel(self, label_text):
-        frame = ctk.CTkFrame(self)
-        self.value_entry_label = ctk.CTkLabel(frame, text=label_text, justify="left", width=LABEL_WIDTH, anchor="w").grid(row=0, column=0, sticky="nw")
-        self.value_entry = ctk.CTkEntry(frame, width=50, placeholder_text="...").grid(row=0, column=1, sticky="nw")
-        return frame
-    
-    def createDropdownLabel(self, label_text, options):
-        frame = ctk.CTkFrame(self)
-        self.dropdown_label = ctk.CTkLabel(frame, text=label_text, justify="left", width=LABEL_WIDTH, anchor="w").grid(row=0, column=0, sticky="nw")
-        self.dropdown = ctk.CTkOptionMenu(frame, values=options, width = 50, height = 20).grid(row=0, column=1, sticky="nw")
-        return frame
     
     def onFocusIn(self, event):
         print(f"Focus")
         
+    @abc.abstractmethod
+    def get_values(self):
+        pass
 
 class EntryLabel(ctk.CTkFrame):
     def __init__(self, master, label_text):
@@ -138,7 +142,10 @@ class HeatStep(StepFrame):
         
         pad = 5
         
-        self.value_dict = {"temperature": 0, "duration": 0}
+        self.value_dict = {"step_type": "heat",
+                           "temperature": 0, 
+                           "duration": 0
+                           }
         
         self.bake_temperature = EntryLabel(self, "Bake Temperature: ")
         self.bake_temperature.grid(row=1, column=0, sticky = "nw", padx=pad, pady=pad)
@@ -147,8 +154,8 @@ class HeatStep(StepFrame):
         self.bake_time.grid(row=2, column=0, sticky = "nw", padx=pad, pady=pad)
         
     def update_values(self):
-        self.value_dict["temperature"] = self.bake_temperature.entry.get()
-        self.value_dict["duration"] = self.bake_time.entry.get()
+        self.value_dict["temperature"] = self.bake_temperature.get_value()
+        self.value_dict["duration"] = self.bake_time.get_value()
     
     def get_values(self):
         self.update_values()
@@ -161,14 +168,23 @@ class SpinStep(StepFrame):
 
         pad = 5
         
-        self.value_dict = {"speed": 0, "duration": 0}
+        self.value_dict = { "step_type": "spin",
+                            "speed": 0, 
+                            "duration": 0}
         
         self.spin_speed = EntryLabel(self, "Spin Speed: ")
         self.spin_speed.grid(row=1, column=0, sticky = "nw", padx=pad, pady=pad)
         
         self.spin_time = EntryLabel(self, "Spin Time: ")
         self.spin_time.grid(row=2, column=0, sticky = "nw", padx=pad, pady=pad)
-        
+    
+    def update_values(self):
+        self.value_dict["speed"] = self.spin_speed.get_value()
+        self.value_dict["duration"] = self.spin_time.get_value()
+    
+    def get_values(self):
+        self.update_values()
+        return self.value_dict
 
 class ApplyStep(StepFrame):
     def __init__(self, master):
@@ -176,29 +192,48 @@ class ApplyStep(StepFrame):
         options = ["Vial 1", "Vial 2", "Vial 3"]
         pad = 5
         
-        self.value_dict = {"source_vial": 0, "duration": 0}
+        self.value_dict = { "step_type": "apply",
+                            "source_vial": 0,
+                            "duration": 0,
+                            "amount": 0}
 
         self.source_vial = DropdownLabel(self, "Source Vial: ", options)
+       
         self.source_vial.grid(row=1, column=0, sticky = "nw", padx=pad, pady=pad)
         
         self.application_duration = EntryLabel(self, "Application duration: ")
         self.application_duration.grid(row=2, column=0, sticky = "nw", padx=pad, pady=pad)
         
+        self.amount = EntryLabel(self, "Amount: ")
+        self.amount.grid(row=3, column=0, sticky = "nw", padx=pad, pady=pad)
+   
+    def update_values(self):
+        self.value_dict["source_vial"] = self.source_vial.get_value()
+        self.value_dict["duration"] = self.application_duration.get_value()
+        self.value_dict["amount"] = self.amount.get_value()
+    
+    def get_values(self):
+        self.update_values()
+        return self.value_dict
     
 class MixStep(StepFrame):
     def __init__(self, master):
         super().__init__(master, "MIX STEP")
-        options = ["Vial 1", "Vial 2", "Vial 3"]
+        self.options = ["Vial 1", "Vial 2", "Vial 3", "Vial 4", "Vial 5"]
         
         pad = 5
+        self.vial_frames = []
         
-        self.value_dict = {"destination_vial": 0,
-                           "source_1": 0, "source_1_amount": 0,
-                           "source_2": 0, "source_2_amount": 0,
-                           "source_3": 0, "source_3_amount": 0
+     
+        self.value_dict = { "step_type": "mix",
+                            "destination_vial": 0,
+                            "sources": [], 
+                            "source_amounts": []
                            }
-        self.vial_list = []
-        self.destination_vial = DropdownLabel(self, "Destination Vial: ", options)
+        
+        
+        
+        self.destination_vial = DropdownLabel(self, "Destination Vial: ", self.options)
         self.destination_vial.grid(row=1, column=0, sticky = "nw", padx=pad, pady=pad)
 
         self.btn_frame = ctk.CTkFrame(self)
@@ -211,28 +246,51 @@ class MixStep(StepFrame):
         self.newVial()
         
     def newVial(self):
-        if(len(self.vial_list) == 3): return
+        if(len(self.vial_frames) == len(self.options)): return
         
         frame = ctk.CTkFrame(self, width=400, height=100, border_color="#1f6aa5", border_width=2)
+        frame.grid(row=3 + len(self.vial_frames), column=0, sticky = "nw", padx=2, pady=2)
+        
+        dropdown = DropdownLabel(frame, "Source Vial: ", ["Vial 1", "Vial 2", "Vial 3"])
+        dropdown.grid(row=0, column=0, sticky = "nw")
+        entry = EntryLabel(frame, "Amount: ")
+        entry.grid(row=1, column=0, sticky = "nw")
         
         
-        self.createDropdownLabel("Vial Number: ", ["1", "2", "3"]).grid(in_=frame, row=0, column=0, sticky = "nw")
-        self.createValueEntryLabel("Amount: ").grid(in_ = frame, row=1, column=0, sticky = "nw")
-        frame.grid(row=3 + len(self.vial_list), column=0, sticky = "nw", padx=2, pady=2)
-        self.vial_list.append(frame)
+        
+        self.vial_frames.append(frame)
         return
     
     def deleteLastVial(self):
-        if(len(self.vial_list) == 2): return
+        if(len(self.vial_frames) == 2): return
         
-        self.vial_list.pop().destroy()
+        self.vial_frames.pop().destroy()
+
+        
+    def update_values(self):
+        self.value_dict["destination_vial"] = self.destination_vial.get_value()
+        source_list = []
+        source_amount_list = []
+        for frame in self.vial_frames:
+            for child in frame.winfo_children():
+                if isinstance(child, DropdownLabel):
+                    source_list.append(child.get_value())
+                elif isinstance(child, EntryLabel):
+                    source_amount_list.append(child.get_value())
+                    
+        self.value_dict["sources"] = source_list
+        self.value_dict["source_amounts"] = source_amount_list
+        
+    def get_values(self):
+        self.update_values()
+        return self.value_dict
     
 
 
 
 if __name__ == "__main__":
     app = ctk.CTk()
-    
+    step_manager = StepManager()
     procedure_frame = ProcedureFrame(app).grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
  
     app.mainloop()
