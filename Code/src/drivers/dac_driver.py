038cceaf2b792b91bf8416fe736c18947e3df5e7
Jem <46539847+jeremy-borden@users.noreply.github.com>
2025-01-29 14:23:47 -0500

    Update dac_driver.py

--- Code/src/drivers/dac_driver.py
+++ Code/src/drivers/dac_driver.py
@@ -1,20 +1,20 @@
-# from smbus2 import SMBus
+from smbus2 import SMBus
 
-# class DAC():
-#     def __init__(self, address, resolution_bits: int = 12):
-#         self.bus = SMBus(1)
-#         self.address = address
-#         self.resolution_bits = resolution_bits
+class DAC():
+    def __init__(self, address, resolution_bits: int = 12):
+        self.bus = SMBus(1)
+        self.address = address
+        self.resolution_bits = resolution_bits
         
-#     def setVoltageLevel(self, level: float):
+    def setVoltageLevel(self, level: float):
+        """Set the voltage of the DAC. 0 means min voltage, 1 means max"""
+        if level < 0:
+            level = 0
+        elif level > 1:
+            level = 1
         
-#         if level < 0:
-#             level = 0
-#         if level > 1:
-#             level = 1
-        
-#         data = int(level * (pow(2, self.resolution_bits) - 1))  # 12-bit resolution, 0-4095
-#         self.bus.write_byte_data(self.address, 0x00, data)
+        data = int(level * (pow(2, self.resolution_bits) - 1))
+        self.bus.write_byte_data(self.address, 0x00, data)
         
 
 class DAC():
@@ -26,8 +26,9 @@ class DAC():
     def setVoltageLevel(self, level: float):
         if level < 0:
             level = 0
-        if level > 1:
+        elif level > 1:
             level = 1
         
         data = int(level * (pow(2, self.resolution_bits) - 1))  # 12-bit resolution, 0-4095
+        
         print(data)
