2d542af1c3f8e3b2d36475a87bc9d6d03db0a20b
Jem <46539847+jeremy-borden@users.noreply.github.com>
2025-02-17 18:07:44 -0500

    updates to pippete assem

--- Code/src/objects/pippete.py
+++ Code/src/objects/pippete.py
@@ -1,9 +1,9 @@
-
 import os
 import sys
-pp=os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
+pp=os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
 sys.path.append(pp)
-from src.drivers.controlboard_driver import ControlBoard
+
+from drivers.controlboard_driver import ControlBoard
 from gpiozero import AngularServo
 from dataclasses import dataclass
 from time import sleep
@@ -11,8 +11,10 @@ from time import sleep
 # TODO add data class representing each pippete? max volume, max dispense distance, full eject distance, last used fluid? etc
 @dataclass
 class Pipette:
-    MAX_VOLUME_UL: int
-    ul_dispensed_per_mm: int
+    MAX_VOLUME_UL: int # the maximum volume the pipette is capable of drawing
+    PLUNGER_TOP_MM: int # location where the actuator is just above the plunger
+    PLUNGER_BOTTOM_MM: int # location where the actuator presses the plunger to its limit
+    DISPENSED_UL_PER_MM: int
     last_fluid: str = None
     
 
@@ -25,21 +27,38 @@ class PipetteHandler():
         self.pipettes = pipettes
         
         
-        self.pipette = None
+        
+        self.current_pipette: Pipette = None
         self.volume_ul
+        
+    def set_pipette(self, index: int):
+        if index > len(self.pipettes):
+            return
+        
+        self.current_pipette = self.pipettes[index]
         
     def extract(self, volume_ul: int):
         
-        # assuming actuyator is right at plunger without depressing
-        # press plunger to volume_ul
-        # lower?
-        # depress plunger and raise
-        pass
+        # calculate the distance needed to push out specified volume
+        distance_mm = volume_ul / self.current_pipette.DISPENSED_UL_PER_MM
+        
+        # pre-depress plunger
+        self.control_board.move_axes(["A"], -distance_mm, 300, False)
+        #lower into vial
+        self.control_board.move_axes(["Y"], -10, 300, True)
+        #un press plunger
+        self.control_board.move_axes(["A"], distance_mm, 300, False)
+        #raise out of vial
+        self.control_board.move_axes(["Y"], 10, 300, True)
+
     
-    def dispense_all(self, duration_seconds: float):
-        # assuming actuyator is right at plunger without depressing
-        # press plunger to max volume
-        # go past to ensre all fluid is out
+    def dispense_all(self, duration_s: float):
+        
+        #calculate distance to fully depress plunger
+        distance_mm = self.current_pipette.PLUNGER_BOTTOM_MM - self.current_pipette.PLUNGER_BOTTOM_MM
+        
+        
+        
         pass
     
     def dock_pipette(self):
@@ -58,8 +77,13 @@ class PipetteHandler():
 if __name__ == "__main__":
     
     # ph = PipetteHandler(None, None, None, 5)
-    p = Pipette(5, 5, "been")
-    print(p)
-    p.MAX_VOLUME_UL = 4
+    p = Pipette(
+        MAX_VOLUME_UL=1000,
+        PLUNGER_TOP_MM=155,
+        PLUNGER_BOTTOM_MM=35,
+        DISPENSED_UL_PER_MM=5)
     
-    print(p)
+    tip_eject_servo = None
+    pipette_holder_servo = None
+    pipette_system = PipetteHandler(None, tip_eject_servo, pipette_holder_servo, [p])
+    
