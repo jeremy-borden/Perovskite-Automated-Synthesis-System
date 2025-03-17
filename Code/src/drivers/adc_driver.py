f9ee017e40938c3f5db24b3bfa238096866cc129
Jem <46539847+jeremy-borden@users.noreply.github.com>
2025-03-17 08:53:03 -0400

    Update adc_driver.py

--- Code/src/drivers/adc_driver.py
+++ Code/src/drivers/adc_driver.py
@@ -6,10 +6,16 @@ import adafruit_max31856
 
 class ADC():
     def __init__(self):
-        spi = board.SPI()
-        cs = digitalio.DigitalInOut(board.D5)
-        cs.direction = digitalio.Direction.OUTPUT
-        self.adc = adafruit_max31856.MAX31856(spi,cs)
+        try:
+            spi = board.SPI()
+            cs = digitalio.DigitalInOut(board.D5)
+            cs.direction = digitalio.Direction.OUTPUT
+            self.adc = adafruit_max31856.MAX31856(spi,cs)
+        except Exception as e:
+            self.adc = None
         
     def get_temperature(self):
+        if self.adc is None:
+            return
+        
         return self.adc.temperature
