c04376ee63a3bebb1c50840bfcee39470162d836
Jem <46539847+jeremy-borden@users.noreply.github.com>
2025-03-17 10:37:20 -0400

    e

--- Code/src/drivers/adc_driver.py
+++ Code/src/drivers/adc_driver.py
@@ -1,17 +1,19 @@
 import board
 import digitalio
 import adafruit_max31856
-
+import logging
 
 
 class ADC():
     def __init__(self):
+        self.logger = logging.getLogger("Main Logger")
         try:
             spi = board.SPI()
             cs = digitalio.DigitalInOut(board.D5)
             cs.direction = digitalio.Direction.OUTPUT
             self.adc = adafruit_max31856.MAX31856(spi,cs)
         except Exception as e:
+            self.logger.error("Failed to connect to ADC")
             self.adc = None
         
     def get_temperature(self):
