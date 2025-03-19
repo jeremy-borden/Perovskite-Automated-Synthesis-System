bcc56933da05c077024d57b7c34b09b6fb3099fb
Jem <46539847+jeremy-borden@users.noreply.github.com>
2025-03-19 10:04:37 -0400

    Update adc_driver.py

--- Code/src/drivers/adc_driver.py
+++ Code/src/drivers/adc_driver.py
@@ -22,7 +22,7 @@ class ADC():
             return
         
         for k in self.adc.fault:
-            self.logger.debug(f"{k}:")
+            self.logger.debug(f"{k}: {self.adc.fault[k]}")
         
         self.adc.initiate_one_shot_measurement()
         while(self.adc.oneshot_pending):
