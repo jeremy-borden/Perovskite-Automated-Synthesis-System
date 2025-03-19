8d94589ec51065596aa52cb0f9ab828ff507b2ef
Jem <46539847+jeremy-borden@users.noreply.github.com>
2025-03-19 09:55:56 -0400

    Update adc_driver.py

--- Code/src/drivers/adc_driver.py
+++ Code/src/drivers/adc_driver.py
@@ -9,7 +9,7 @@ class ADC():
         self.logger = logging.getLogger("Main Logger")
         try:
             spi = board.SPI()
-            cs = digitalio.DigitalInOut(board.D8)
+            cs = digitalio.DigitalInOut(board.D5)
             cs.direction = digitalio.Direction.OUTPUT
             self.adc = adafruit_max31856.MAX31856(spi,cs)
         except Exception as e:
@@ -20,9 +20,9 @@ class ADC():
         
         if self.adc is None:
             return
+        
         self.adc.initiate_one_shot_measurement()
         while(self.adc.oneshot_pending):
             pass
         t = self.adc.unpack_temperature()
-        self.logger.debug(f"Temp: {t}")
         return t
