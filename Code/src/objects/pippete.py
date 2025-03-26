35268b5c364c56f641f71f1224b0706f27da4040
Jem <46539847+jeremy-borden@users.noreply.github.com>
2025-03-26 11:55:41 -0400

    Update pippete.py

--- Code/src/objects/pippete.py
+++ Code/src/objects/pippete.py
@@ -90,8 +90,8 @@ class PipetteHandler():
         self.control_board.move_axis("B",  volume_ul*ul_per_mm, feedrate, True)
         
     def eject_tip(self):
-        self.tip_eject_servo.angle = 60
-        sleep(0.5)
+        self.tip_eject_servo.angle = 120
+        sleep(2)
         self.tip_eject_servo.angle = 0
          
     def open_grabber(self):
