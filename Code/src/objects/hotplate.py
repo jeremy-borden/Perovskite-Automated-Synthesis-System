dd1b804baa69cb99d1fb96264a2bc03dd549e3a7
Boston-Powers <gavinrobert02@gmail.com>
2025-03-27 16:02:17 -0400

    Update hotplate.py
    Signed-off-by: Boston-Powers <gavinrobert02@gmail.com>

--- Code/src/objects/hotplate.py
+++ Code/src/objects/hotplate.py
@@ -57,7 +57,7 @@ import logging
 class Hotplate(threading.Thread):
     MAX_TEMPERATURE_C = 540
 
-    def __init__(self, serial_port="/dev/ttyUSB1", baud_rate=115200):
+    def __init__(self, serial_port="/dev/ttyACM1", baud_rate=115200):
         super().__init__(name="Hotplate", daemon=True)
         self.serial_port = serial.Serial(serial_port, baud_rate, timeout=1)
         self._current_temperature_c = 0
