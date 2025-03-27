b9b8eaf23818f6079e5a60887f1288bc26474bd7
Jem <46539847+jeremy-borden@users.noreply.github.com>
2025-03-27 19:42:32 -0400

    Update gripper.py

--- Code/src/objects/gripper.py
+++ Code/src/objects/gripper.py
@@ -9,7 +9,7 @@ class Gripper():
         self.finger_servo.angle = 22
     
     def close(self):
-        self.finger_servo.angle = 6
+        self.finger_servo.angle = 2
     
     def set_arm_angle(self, angle_degrees:int):
         self.arm_servo.angle = angle_degrees
