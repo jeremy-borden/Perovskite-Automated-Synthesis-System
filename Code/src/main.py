0f223fe2c93dce210cdc91b7901402d197a17d20
Boston-Powers <gavinrobert02@gmail.com>
2025-03-28 10:37:21 -0400

    Update main.py
    Signed-off-by: Boston-Powers <gavinrobert02@gmail.com>

--- Code/src/main.py
+++ Code/src/main.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
     logger.setLevel(logging.DEBUG)
     
     # -- CONTROL BOARD --
-    control_board = ControlBoard(com_port="/dev/ttyACM1",logger=logger)
+    control_board = ControlBoard(com_port="/dev/ttyACM0",logger=logger)
     
     # -- TOOLHEAD --
     toolhead = Toolhead(control_board=control_board)
@@ -75,7 +75,7 @@ if __name__ == "__main__":
     # -- HOTPLATE --
     dac = DAC()
     adc = ADC()
-    hotplate = Hotplate(serial_port="/dev/ttyACM0", baud_rate=115200)
+    hotplate = Hotplate(serial_port="/dev/ttyACM1", baud_rate=115200)
     hotplate.start()
     #hotplate = None
     # -- SPECTROMETER + INFEED --
