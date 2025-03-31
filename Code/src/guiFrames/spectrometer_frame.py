6e8e896cde9a3d8cae703354ecd5ca1ee8a5f80c
stever0shan <ssurendrakum@binghamton.edu>
2025-03-30 21:28:38 -0400

    Update spectrometer_frame.py
    Signed-off-by: stever0shan <ssurendrakum@binghamton.edu>

--- Code/src/guiFrames/spectrometer_frame.py
+++ Code/src/guiFrames/spectrometer_frame.py
@@ -79,4 +79,5 @@ class SpectrometerFrame(ctk.CTkFrame):
         except Exception as e:
             self.status_label.configure(text=f"Error: {str(e)}")
             
-        self.after(1000, self.update_plot)
+        if self.winfo_exists():
+            self.after(1000, self.update_plot)
