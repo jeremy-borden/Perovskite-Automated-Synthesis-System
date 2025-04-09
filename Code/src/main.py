8746de836ba899273d080cdfb145a3219756bdfe
Jem <46539847+jeremy-borden@users.noreply.github.com>
2025-04-09 13:29:25 -0400

    Update main.py

--- Code/src/main.py
+++ Code/src/main.py
@@ -64,6 +64,7 @@ if __name__ == "__main__":
     # -- PIPETTE HANDLER --
     tip_eject_servo = AngularServo(pin=27, min_angle=0, max_angle=270, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000)
     grabber_servo = AngularServo(pin=22, min_angle=0, max_angle=180, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000)
+    grabber_servo.angle = 180
     pipettes = [Pipette(200, 22, 4.5, 2.5, False),
                 Pipette(1000, 22, 4.5, 2.5, True)]
     pipette_handler = PipetteHandler(control_board=control_board,
