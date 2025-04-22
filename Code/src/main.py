ecc2d9f7bd5b65a1b1288b418857f944d0da5db3
stever0shan <ssurendrakum@binghamton.edu>
2025-04-22 12:36:05 -0400

    Update main.py
    Signed-off-by: stever0shan <ssurendrakum@binghamton.edu>

--- Code/src/main.py
+++ Code/src/main.py
@@ -27,7 +27,7 @@ from guiFrames.conection_frame import ConnectionFrame
 from guiFrames.procedure_builder_frame import ProcedureBuilderFrame
 from guiFrames.spectrometer_frame import SpectrometerFrame
 from guiFrames.locations_frame import LocationFrame
-from src.guiFrames.ml_model_frame import MLModelFrame
+# from src.guiFrames.ml_model_frame import MLModelFrame
 
 
 from procedure_handler import ProcedureHandler
@@ -106,7 +106,7 @@ if __name__ == "__main__":
                             spectrometer=spectrometer,
                             vial_carousel=vial_carousel,
                             pippete_handler=pipette_handler,
-                            #spectrometer_frame=spectrometer_frame, 
+                            spectrometer_frame=spectrometer_frame, 
                             tip_matrix=tip_matrix)
     
     procedure_handler = ProcedureHandler(dispatcher=dispatcher)
@@ -137,13 +137,13 @@ if __name__ == "__main__":
     # putting the frames on the gui
     procedure_frame.grid(row=0, column=0, padx=5, pady=5,sticky="nsew")
     connection_frame.grid(row=0, column=1, padx=5, pady=5, sticky="nsew")
-    #spectrometer_frame.grid(row=0, column=2, padx=10, pady=10, sticky="nsew")
+    spectrometer_frame.grid(row=0, column=2, padx=10, pady=10, sticky="nsew")
     console_frame.grid(row=1, column=0, padx=5, pady=5,sticky="nsew")
     procedure_builder_frame.grid(row=1, column=1, rowspan=2, sticky="nsew")
     camera_frame.grid(row=1, column=2, padx=5, pady=5,sticky="new")
     info_frame.grid(row=2, column=0, padx=5, pady=5, sticky="new")
     location_frame.grid(row=2, column=2,padx=5, pady=5, sticky="new")
-    ml_model_frame.grid(row=0, column=2, padx=10, pady=10, sticky="nsew")
+    # ml_model_frame.grid(row=0, column=2, padx=10, pady=10, sticky="nsew")
     #ml_model_frame.grid(row=3, column=0, columnspan=3, padx=10, pady=10, sticky="nsew")
     
 
