2553de6412bf31dfe6169d30eafc2be5daeca39e
stever0shan <ssurendrakum@binghamton.edu>
2025-04-24 18:50:39 -0400

    Update main.py
    Signed-off-by: stever0shan <ssurendrakum@binghamton.edu>

--- Code/src/main.py
+++ Code/src/main.py
@@ -10,7 +10,7 @@ from drivers.spincoater_driver import SpinCoater
 from drivers.camera_driver import Camera
 from drivers.procedure_file_driver import ProcedureFile
 from drivers.spectrometer_driver import Spectrometer
-from drivers import ml_driver
+# from drivers import ml_driver
 
 # -- OBJECT IMPORT --
 
@@ -30,7 +30,7 @@ from guiFrames.conection_frame import ConnectionFrame
 from guiFrames.procedure_builder_frame import ProcedureBuilderFrame
 from guiFrames.spectrometer_frame import SpectrometerFrame
 from guiFrames.locations_frame import LocationFrame
-from guiFrames.ml_model_frame import MLModelFrame
+# from guiFrames.ml_model_frame import MLModelFrame
 
 
 from procedure_handler import ProcedureHandler
@@ -142,7 +142,7 @@ if __name__ == "__main__":
     info_frame = InfoFrame(app, control_board, hotplate, pipette_handler, vial_carousel)
     procedure_builder_frame = ProcedureBuilderFrame(app, dispatcher.move_dict, procedure_handler)
     location_frame = LocationFrame(master=app)
-    ml_model_frame = MLModelFrame(app, width=370)
+    # ml_model_frame = MLModelFrame(app, width=370)
     
     # putting the frames on the gui
     procedure_frame.grid(row=0, column=0, padx=5, pady=5,sticky="nsew")
@@ -153,8 +153,7 @@ if __name__ == "__main__":
     camera_frame.grid(row=1, column=2, padx=5, pady=5,sticky="new")
     info_frame.grid(row=2, column=0, padx=5, pady=5, sticky="new")
     location_frame.grid(row=2, column=2,padx=5, pady=5, sticky="new")
-    # ml_model_frame.grid(row=0, column=2, padx=10, pady=10, sticky="nsew")
-    ml_model_frame.grid(row=1, column=2, padx=5, pady=5,sticky="new")
+    # ml_model_frame.grid(row=1, column=2, padx=5, pady=5,sticky="new")
 
     # run the gui
     hotplate.set_temperature(0)
